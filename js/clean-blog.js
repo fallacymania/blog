e57bc23898f9551e97ef0b22d5735113f8d03ecb
Yuri Baranov <baranovu@gmail.com>
2017-10-01 01:26:08 +0300

    Clean Blog by Start Bootstrap template translation and adaptation for fallacymania.github.io/blog

--- js/clean-blog.js
+++ js/clean-blog.js
@@ -46,7 +46,7 @@ $(function() {
                     $('#success > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
                         .append("</button>");
                     $('#success > .alert-success')
-                        .append("<strong>Your message has been sent. </strong>");
+                        .append("<strong>Ваше сообщение отослано. </strong>");
                     $('#success > .alert-success')
                         .append('</div>');
 
@@ -58,7 +58,7 @@ $(function() {
                     $('#success').html("<div class='alert alert-danger'>");
                     $('#success > .alert-danger').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
                         .append("</button>");
-                    $('#success > .alert-danger').append("<strong>Sorry " + firstName + ", it seems that my mail server is not responding. Please try again later!");
+                    $('#success > .alert-danger').append("<strong>Приносим извинения, " + firstName + ", похоже что сервер передачи сообщений сейчас не функционирует! Повторите попытку позже.");
                     $('#success > .alert-danger').append('</div>');
                     //clear all fields
                     $('#contactForm').trigger("reset");
